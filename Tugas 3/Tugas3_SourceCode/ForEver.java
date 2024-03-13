a32b2eee18bb2974672783a23e12435a0e3348a0
Achmad Rizky M <160821074+achrizky05@users.noreply.github.com>
2024-03-13 15:26:25 +0800

    Add files via upload

---
+++ Tugas 3/Tugas3_SourceCode/ForEver.java
@@ -0,0 +1,17 @@
+/*
+NIM : 13020220120
+Nama : Achmad Rizky Maulana
+Hari/Tanggal : Rabu 13/03/2024
+Waktu Pengerjaan : 08:30
+*/
+
+ public class ForEver {
+
+    public static void main(String[] args) {
+
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true) {
+            System.out.print("Print satu baris ..... \n");
+        }
+    }
+}
